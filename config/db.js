ccd242438c728346fa55c26069d27bb2a68e6f47
adamgofast <adam@gofastcrushgoals.com>
2025-05-29 00:25:42 +0000

    Updated for mongo push

--- config/db.js
+++ config/db.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "GoFastFamily",  // 👈 Custom DB name — totally valid
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected...");
+    console.log("MongoDB connected to GoFastFamily...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
