7e0b320faead37514c17403985bd252469873547
adamgofast <adam@gofastcrushgoals.com>
2025-06-04 02:03:07 +0000

    change user trip service

--- services/TripWell/userTripService.js
+++ services/TripWell/userTripService.js
@@ -1,24 +1,26 @@
 const User = require("../../models/User");
 
-// 🔗 Link trip _id to user model
-async function setUserTrip(firebaseId, tripId) {
-  const user = await User.findOneAndUpdate(
-    { firebaseId },
-    { tripId },
+// 🔗 Link trip _id to user by userId (ObjectId)
+async function setUserTrip(userId, tripId) {
+  if (!tripId) throw new Error("Missing tripId");
+
+  const user = await User.findByIdAndUpdate(
+    userId,
+    { tripId: tripId.toString() }, // just to ensure cleanliness
     { new: true }
   );
 
-  if (!user) throw new Error(`User not found for firebaseId: ${firebaseId}`);
+  if (!user) throw new Error(`User not found for _id: ${userId}`);
   return user;
 }
 
-// 💤 Archive trip when finished or user leaves trip
-async function archiveTrip(firebaseId, tripId) {
-  return await User.findOneAndUpdate(
-    { firebaseId },
+// 💤 Archive trip
+async function archiveTrip(userId, tripId) {
+  return await User.findByIdAndUpdate(
+    userId,
     {
       tripId: null,
-      pastTripId: tripId
+      pastTripId: tripId.toString()
     },
     { new: true }
   );
