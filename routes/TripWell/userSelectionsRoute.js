ad989ed67a610e675a0559cd6910e01734e18226
adamgofast <adam@gofastcrushgoals.com>
2025-09-18 22:04:23 -0400

    trip intent fix

--- routes/TripWell/userSelectionsRoute.js
+++ routes/TripWell/userSelectionsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyFirebaseToken } = require('../../middleware/verifyFirebaseToken');
+const verifyFirebaseToken = require('../../middleware/verifyFirebaseToken');
 const { 
   saveMetaSelections, 
   saveSampleSelections, 
